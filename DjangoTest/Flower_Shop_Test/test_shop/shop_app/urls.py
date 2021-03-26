8b7c316f2c98513c2529aeb047159ca3a24537e5
Kong Fanyu <fanyu.kong@ucdconnect.ie>
2021-03-26 21:44:59 +0800

    split base and some pages (uncompleted)

--- DjangoTest/Flower_Shop_Test/test_shop/shop_app/urls.py
+++ DjangoTest/Flower_Shop_Test/test_shop/shop_app/urls.py
@@ -6,5 +6,5 @@ app_name = 'shop_app'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('product_detail/<int:product_id>/', views.product, name='detail'),
+    path('<int:product_id>/', views.product, name='detail'),
 ]
